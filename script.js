888c5f92f22997ccb1029b2530de904e241182f0
unknown <salomao.junior2k22@gmail.com>
2023-10-19 12:20:40 -0300

    html e script alterado -

--- script.js
+++ script.js
@@ -8,24 +8,20 @@ menu.addEventListener('click', () => {
 
 // Função para carregar e exibir dados da planilha do Google Sheets
 function loadGoogleSheetData() {
-    // ID da planilha do Google Sheets
-    const spreadsheetId = '1BceSwwl-e4k-poICuKSAVAj-kMlGH2kEUKRYN__oFWI';
-    // ID da planilha dentro do documento (geralmente 0 para a primeira planilha)
+    const spreadsheetId = '';
     const sheetId = 0;
 
     gapi.client.sheets.spreadsheets.values.get({
         spreadsheetId: spreadsheetId,
-        range: 'infos' // Substitua pelo nome da aba que você deseja ler
+        range: ''
     }).then(function (response) {
         const data = response.result.values;
         const tableBody = document.querySelector('#actions-table tbody');
 
-        // Limpe qualquer conteúdo existente na tabela
         tableBody.innerHTML = '';
 
-        // Preencha a tabela com os dados da planilha
         data.forEach(function (row) {
-            const rowData = row.map(item => item || ''); // Lida com valores nulos ou indefinidos
+            const rowData = row.map(item => item || '');
 
             const tableRow = document.createElement('tr');
             rowData.forEach(function (cellData) {
@@ -39,15 +35,13 @@ function loadGoogleSheetData() {
     });
 }
 
-// Função para inicializar a API do Google Sheets
 function initGoogleSheetsApi() {
     gapi.client.init({
-        apiKey: 'AIzaSyDShz3JoH-9LtPf-fAsngo-O2r_2mAICb4',
+        apiKey: '',
         discoveryDocs: ['https://sheets.googleapis.com/$discovery/rest?version=v4'],
     }).then(function () {
         loadGoogleSheetData();
     });
 }
 
-// Carrega a API do Google Sheets e inicia a aplicação
 gapi.load('client', initGoogleSheetsApi);
